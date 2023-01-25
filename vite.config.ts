a8dbb748b4376fd76aa052302e4508cb75770c04
richardebrain <richardebrain20@gmail.com>
2023-01-25 16:24:19 +0100

    feat : another config

--- vite.config.ts
+++ vite.config.ts
@@ -8,16 +8,26 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  plugins: [viteSvgr(), react(),tsconfigPaths()],
+  plugins: [viteSvgr(), react(), tsconfigPaths()],
   resolve: {
     alias: {
       'node-fetch': 'isomorphic-fetch',
     },
   },
-build:{
-  rollupOptions: {
- 
-    // rollup external on svg files and absolute paths
+  build: {
+    rollupOptions: {
+
+      // rollup external on svg files and absolute paths
+      output: {
+
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+      }
 
     }
   },
